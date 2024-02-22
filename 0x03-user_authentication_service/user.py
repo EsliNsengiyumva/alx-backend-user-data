b4026bd5ab24c1d27603143f87446571fd8584f8
Esli Nsengiyumva <47382543+EsliNsengiyumva@users.noreply.github.com>
2024-02-22 16:06:04 +0200

    Update user.py

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,23 +1,29 @@
 #!/usr/bin/env python3
-
 """
-Module defining SQLAlchemy User model for the users table.
+User Module
 """
 
-from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
 
 Base = declarative_base()
 
 
 class User(Base):
     """
-    SQLAlchemy model for the users table.
+    User class
     """
+
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
-    email = Column(String, nullable=False)
-    hashed_password = Column(String, nullable=False)
-    session_id = Column(String, nullable=True)
-    reset_token = Column(String, nullable=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
+
+    def __repr__(self):
+        """
+        String Rep
+        """
+        return f"User: id={self.id}"
